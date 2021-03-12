42502962e45ee5a5726c2207ee9c97010566eb40
TimTeillor <timteillor@gmail.com>
2021-03-12 04:03:54 +0300

    var, val, type, expressions

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,26 @@
+var hello = "Hello World"
+var number_1 = 85
+var number_2 = 5
+
 fun main(args: Array<String>) {
-    println("Tim")
-    println("Teillor")
+    var age: Int? = null
+
+    age = 44
+
+    println("Переменаая: $age")
+
+    var name = "John"
+    var firstChar = 's'
+
+    var isTrue: Boolean = true
+    var num: Float = 4.5f
+    var num_2: Double = 3.2353434
+
+    val number: Int = 6
+
+    var res = number_1 + number_2
+    res %= 10
+    res--
+
+    println("Результат: $res")
 }
