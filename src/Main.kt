72a10543944ca13004fbc61c66f953af5693aa13
TimTeillor <timteillor@gmail.com>
2021-03-13 00:33:30 +0300

    Create functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,39 +1,16 @@
+fun sayHello(first: String, number: Int) {
+    println("$first - $number")
+}
+
+fun add(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun saySomething(first: Boolean): String = "Hello world $first"
+
 fun main(args: Array<String>) {
-    var word: String? = null
-    var isHasCar: Boolean = true
-
-//    word = "Hello"
-
-    if(word == null) {
-        println("Переменная word равна значению null")
-
-        if(!isHasCar || word == null)
-            println("Что-то сработало")
-    } else if(word == "Hello")
-        println("Переменная: $word")
-    else
-        print("Переменная word не равна значению null")
-
-    val number: Int = 50
-    when(number) {
-        null -> print("Переменная равна null")
-        10 -> print("Переменная равна 10")
-        6 -> print("Переменная равна 6")
-        190 -> print("Переменная равна 190")
-        5 -> print("Переменная равна 5")
-        else -> print("Переменная какая-то непонятная")
-    }
-
-//    var res = 0
-//    if(number > 30)
-//        res = 1
-//    else
-//        res = 0
-
-    var res = if(number > 30) 1 else 0
-
-    val words = when(number) {
-        null -> "Ничего"
-        else -> number
-    }
+    sayHello("Hello", 13)
+    val result = add(4, 16)
+    println(result)
+    println(saySomething(true))
 }
