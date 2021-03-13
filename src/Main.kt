9341929bd64f437650fc47c1e73e2a7855951164
TimTeillor <timteillor@gmail.com>
2021-03-13 14:37:04 +0300

    Add array

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,36 @@
-fun sayHello(first: String, number: Int) {
-    println("$first - $number")
+fun printArray(items: Map<String, Any>) {
+    items.forEach { key, value ->
+        println("$key -> $value")
+    }
 }
-
-fun add(x: Int, y: Int): Int {
-    return x + y
-}
-
-fun saySomething(first: Boolean): String = "Hello world $first"
 
 fun main(args: Array<String>) {
-    sayHello("Hello", 13)
-    val result = add(4, 16)
-    println(result)
-    println(saySomething(true))
+    var items: Array<Any> = arrayOf(4, 5, 7, 1, 2, 0, "Hello", true)
+//    var items_list = listOf(4, 5, 7, 1, 2, 0)
+    var items_list = mutableListOf<Any>(4, 5, 7, 1, 2, 0)
+//    val someArray = arrayOf(true, false)
+//    println(items.set(0, 11))
+//    println(items[0])
+//    println(items.get(1))
+//    println(items.size)
+
+    var dogs = listOf<Any>("Bob", "Charley")
+
+    items_list.add("Dogs")
+    items_list.addAll(4, dogs)
+
+//    var user = mapOf("name" to "Bob", "age" to 1, "isHasCar" to true)
+    var user = mutableMapOf<String, Any>("name" to "Bob", "age" to 1, "isHasCar" to true)
+    user.put("city", "New York")
+
+    printArray(user)
+
+//    for(el in items)
+//        println(el)
+
+//    items.forEachIndexed { index, el ->
+//        println("$el с индексом $index")
+//    }
+
+//    user.forEach { (key, value) -> println("$key -> $value") }
 }
